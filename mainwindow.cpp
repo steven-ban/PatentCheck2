1988bc00a3d41b276424dc71ee543f833f157867
Steven <banguijun@sina.com>
2018-02-01 16:05:20 +0800

    加入分割提取权利要求书及说明书的内容
    使用正则表达式分割提取权利要求书及说明书的内容

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,7 @@
 
 #include<QSettings>
 #include<QMessageBox>
+#include<QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -16,10 +17,21 @@ MainWindow::MainWindow(QWidget *parent) :
 
     connect(ui->actionSettings_2, SIGNAL(triggered()), this, SLOT(showSettings()));
     connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(check()));
+    connect(ui->generateButton, SIGNAL(clicked()), this, SLOT(generateDecision()));
 
 }
 
 void MainWindow::initPatent(){
+    // 使用正则表达式来分割权利要求，得到权利要求列表
+    QRegularExpression claimRx("^(\\d+).", QRegularExpression::MultilineOption);
+    this->claimList = ui->claimsTextEdit->toPlainText().split(claimRx);
+    qDebug()<<"Claims: "<<ui->claimsTextEdit->toPlainText();
+    qDebug()<<"Claim number: "<<this->claimList.count();
+
+    // 使用正则表达式来分割说明书，得到说明书段落列表
+    QRegularExpression descriptionRx("^\\[(\\d){4}\\]", QRegularExpression::MultilineOption);
+    this->descriptionList = ui->descriptionTextEdit->toPlainText().split(descriptionRx);
+    qDebug()<<"Description number: "<<this->descriptionList.count();
 
 }
 
@@ -46,10 +58,17 @@ void MainWindow::check(){
         return;
     }
 
+    // 从输入的文本中提取信息
+    this->initPatent();
 
+
+
+
+}
+
+void MainWindow::generateDecision(){
     // 生成通知书
     textDlg->show();
-
 }
 
 void MainWindow::showSettings(){
